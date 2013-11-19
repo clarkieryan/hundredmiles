9352bab106e2e47146832b4de65254b651200789
Ryan Clarke <clarkie.ryan@gmail.com>
2013-11-19 19:06:00 +0000

    Added in base rails stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hundredmiles::Application.config.secret_key_base = 'aab458d6ea05c29c9294f397cf1e9c5036bf85685ff269319f430e826db3b8f1c2b895afe3ee30dd80884ccd7ba71df71c371ac4efe79bdff4207c4501371c3d'
